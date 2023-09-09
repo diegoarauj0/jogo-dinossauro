913e9209e0954fe61c036dbbcb17a77580aafa28
Diego Araujo <97152724+diegoarauj0@users.noreply.github.com>
2023-09-09 03:23:29 -0300

    Adicionando o obstáculos

--- js/index.js
+++ js/index.js
@@ -45,6 +45,7 @@ class Player {
     constructor(props) {
         this._speed = { x: 0, y: 0 };
         this._position = props.position;
+        this._origin = { x: props.position.x, y: props.position.y };
         this._size = props.size;
         this._force = props.force;
         this._gravity = props.gravity;
@@ -64,11 +65,23 @@ class Player {
             this._position.y = canvasSize.height - this._size.height;
         }
     }
+    Reset() {
+        this._speed = { x: 0, y: 0 };
+        this._position = { x: this._origin.x, y: this._origin.y };
+    }
     Jump(canvasSize) {
         if (this._position.y + this._size.height < canvasSize.height)
             return;
         this._speed.y = 0;
         this._speed.y -= this._force;
+    }
+    Collision(obstacle) {
+        let CollisionX = obstacle.X + obstacle.Width > this.X && obstacle.X < this.X + this.Width;
+        let CollisionY = this.Y + this.Height > obstacle.Y;
+        if (CollisionX && CollisionY) {
+            return true;
+        }
+        return false;
     }
 }
 class Render {
@@ -107,6 +120,10 @@ class Render {
         this._context.fillStyle = "black";
         this._context.fillRect(player.X, player.Y, player.Width, player.Height);
     }
+    Obstacle(obstacle) {
+        this._context.fillStyle = "black";
+        this._context.fillRect(obstacle.X, obstacle.Y, obstacle.Width, obstacle.Height);
+    }
     Loading(color) {
         this._context.fillStyle = color;
         this._context.textAlign = "center";
@@ -117,16 +134,64 @@ class Render {
         this._context.textAlign = "center";
         this._context.fillText("Error ...", this.Width / 2, this.Height / 2);
     }
+    GameOver(color) {
+        this._context.fillStyle = color;
+        this._context.textAlign = "center";
+        this._context.fillText("GameOver", this.Width / 2, this.Height / 2);
+    }
+}
+class Obstacle {
+    constructor(props) {
+        this._size = props.size;
+        this._position = props.position;
+    }
+    get X() { return this._position.x; }
+    get Y() { return this._position.y; }
+    get Position() { return this._position; }
+    get Size() { return this._size; }
+    get Width() { return this._size.width; }
+    get Height() { return this._size.height; }
+    Refresh(speed) {
+        this._position.x += speed;
+    }
 }
 class Game {
     constructor(props) {
         this._on = false;
+        this._gameOver = false;
+        this._obstacles = [];
+        this._frames = {
+            addObstacleSpeed: 0,
+            spawnObstacle: 0
+        };
         this.render = new Render(props.renderProps);
         this.player = new Player(props.playerProps);
+        this._opticObstacle = {
+            spawn: {
+                frame: props.obstacleOption.spawn.frame,
+                value: 0,
+                max: props.obstacleOption.spawn.max
+            },
+            speed: {
+                add: props.obstacleOption.speed.add,
+                default: props.obstacleOption.speed.value,
+                frame: props.obstacleOption.speed.frame,
+                max: props.obstacleOption.speed.max,
+                value: props.obstacleOption.speed.value
+            },
+            size: props.obstacleOption.size
+        };
         this.control = new Control({
             keys: [
                 { code: props.control.jump, cb: () => {
+                        if (this._gameOver == true)
+                            return;
                         this.player.Jump(this.render.Size);
+                    } },
+                { code: "KeyR", cb: () => {
+                        if (this._gameOver == false)
+                            return;
+                        this.Reset();
                     } }
             ]
         });
@@ -150,22 +215,80 @@ class Game {
             }
         }));
     }
+    Reset() {
+        this._gameOver = false;
+        this.player.Reset();
+        this._frames.addObstacleSpeed = 0;
+        this._frames.spawnObstacle = 0;
+        this._opticObstacle.speed.value = this._opticObstacle.speed.default;
+        this._obstacles = [];
+    }
     Refresh() {
         if (!this._on)
             throw "";
-        this.player.Refresh(this.render.Size);
+        this.Logic();
+        this.Render();
+    }
+    Logic() {
         this.control.Refresh();
-        //Render
+        if (this._gameOver)
+            return;
+        this._frames.addObstacleSpeed++;
+        this._frames.spawnObstacle++;
+        if (this._frames.spawnObstacle >= this._opticObstacle.spawn.frame) {
+            this._frames.spawnObstacle = 0;
+            let random = Math.floor(Math.random() * 3);
+            this._opticObstacle.spawn.value++;
+            if (random == 0 || this._opticObstacle.spawn.value > this._opticObstacle.spawn.max) {
+                this._opticObstacle.spawn.value = 0;
+                this.AddObstacle();
+            }
+        }
+        if (this._frames.addObstacleSpeed >= this._opticObstacle.speed.frame) {
+            this._frames.addObstacleSpeed = 0;
+            this._opticObstacle.speed.value = this._opticObstacle.speed.value >= this._opticObstacle.speed.max ? this._opticObstacle.speed.max : this._opticObstacle.speed.value + this._opticObstacle.speed.add;
+        }
+        let obstacles = [];
+        this._obstacles.forEach((obstacle, index) => {
+            if (obstacle.X + obstacle.Width > 0) {
+                obstacle.Refresh(-this._opticObstacle.speed.value);
+                obstacles.push(obstacle);
+                if (this.player.Collision(obstacle)) {
+                    this._gameOver = true;
+                }
+            }
+        });
+        this._obstacles = obstacles;
+        obstacles = [];
+        this.player.Refresh(this.render.Size);
+    }
+    Render() {
         this.render.Background("#202124");
+        if (this._gameOver) {
+            this.render.GameOver("red");
+            return;
+        }
         this.render.Player(this.player);
+        this._obstacles.forEach((obstacle) => { this.render.Obstacle(obstacle); });
+    }
+    AddObstacle() {
+        if (!this._on)
+            throw "";
+        this._obstacles.push(new Obstacle({
+            position: {
+                y: this.render.Height - this._opticObstacle.size.height,
+                x: this.render.Width
+            },
+            size: this._opticObstacle.size
+        }));
     }
 }
 const game = new Game({
     playerProps: {
         position: { x: 50, y: 50 },
         size: { width: 50, height: 50 },
-        force: 16,
-        gravity: 0.8
+        force: 14,
+        gravity: 0.6
     },
     renderProps: {
         size: {
@@ -175,6 +298,22 @@ const game = new Game({
     },
     control: {
         jump: "Space"
+    },
+    obstacleOption: {
+        speed: {
+            value: 5,
+            max: 30,
+            add: 1,
+            frame: 250
+        },
+        spawn: {
+            frame: 110,
+            max: 2
+        },
+        size: {
+            width: 50,
+            height: 50
+        }
     },
     element: document.querySelector("main") || document.body
 });
--- src/index.ts
+++ src/index.ts
@@ -27,9 +27,43 @@ interface GameProps {
     element:HTMLElement
     renderProps:RenderProps
     playerProps:PlayerProps
+    obstacleOption:{
+        spawn: {
+            frame:number,
+            max:number
+        },
+        speed:{
+            value:number,
+            max:number,
+            add:number,
+            frame:number
+        },
+        size:Size
+    }
     control:{ 
         jump:string
     }
+}
+
+interface ObstacleProps {
+    size:Size
+    position:Position
+}
+
+interface ObstacleOption {
+    speed:{
+        default:number
+        value:number
+        max:number
+        add:number
+        frame:number
+    }
+    spawn:{
+        frame:number,
+        max:number,
+        value:number
+    },
+    size:Size
 }
 
 class Control {
@@ -82,6 +116,7 @@ class Control {
 class Player {
 
     private _position:Position
+    private readonly _origin:Position
     private _speed:Position = { x:0, y:0 }
     private _size:Size
     private _gravity:number
@@ -89,6 +124,7 @@ class Player {
 
     constructor(props:PlayerProps) {
         this._position = props.position
+        this._origin = { x:props.position.x, y:props.position.y }
         this._size = props.size
         this._force = props.force
         this._gravity = props.gravity
@@ -114,12 +150,30 @@ class Player {
 
     }
 
+    public Reset(): void {
+        this._speed = { x:0, y:0 }
+        this._position = { x:this._origin.x, y:this._origin.y}
+
+    }
+
     public Jump(canvasSize:Size): void {
 
         if (this._position.y + this._size.height < canvasSize.height) return
         this._speed.y = 0
         this._speed.y -= this._force
 
+    }
+
+    public Collision(obstacle:Obstacle): boolean {
+
+        let CollisionX = obstacle.X + obstacle.Width > this.X && obstacle.X < this.X + this.Width
+        let CollisionY = this.Y + this.Height > obstacle.Y
+
+        if ( CollisionX && CollisionY ) {
+            return true
+        }
+
+        return false
     }
 }
 
@@ -171,6 +225,11 @@ class Render {
         this._context.fillRect(player.X, player.Y, player.Width, player.Height)
     }
 
+    public Obstacle(obstacle:Obstacle): void {
+        this._context.fillStyle = "black"
+        this._context.fillRect(obstacle.X, obstacle.Y, obstacle.Width, obstacle.Height)
+    }
+
     public Loading(color:string): void {
         this._context.fillStyle = color
         this._context.textAlign = "center"
@@ -182,24 +241,80 @@ class Render {
         this._context.textAlign = "center"
         this._context.fillText("Error ...", this.Width / 2, this.Height / 2)
     }
+
+    public GameOver(color:string): void {
+        this._context.fillStyle = color
+        this._context.textAlign = "center"
+        this._context.fillText("GameOver", this.Width / 2, this.Height / 2)
+    }
+}
+
+class Obstacle {
+
+    private _position:Position
+    private _size:Size
+
+    constructor(props:ObstacleProps) {
+        this._size = props.size
+        this._position = props.position
+    }
+
+    get X(): number { return this._position.x }
+    get Y(): number { return this._position.y }
+    get Position(): Position { return this._position }
+    get Size(): Size { return this._size }
+    get Width(): number { return this._size.width }
+    get Height(): number { return this._size.height }
+
+    public Refresh(speed:number): void {
+
+        this._position.x += speed
+    }
 }
 
 class Game {
 
     private _on:boolean = false
+    private _gameOver:boolean = false
     private render:Render
     private player:Player
     private control:Control
+    private _obstacles:Obstacle[] = []
+    private _opticObstacle:ObstacleOption
+    private _frames:{ spawnObstacle:number, addObstacleSpeed:number } = {
+        addObstacleSpeed:0,
+        spawnObstacle:0
+    }
 
     constructor(props:GameProps) {
         
         this.render = new Render(props.renderProps)
         this.player = new Player(props.playerProps)
+        this._opticObstacle = {
+            spawn:{
+                frame:props.obstacleOption.spawn.frame,
+                value:0,
+                max:props.obstacleOption.spawn.max
+            },
+            speed:{
+                add:props.obstacleOption.speed.add,
+                default:props.obstacleOption.speed.value,
+                frame:props.obstacleOption.speed.frame,
+                max:props.obstacleOption.speed.max,
+                value:props.obstacleOption.speed.value
+            },
+            size:props.obstacleOption.size
+        }
         this.control = new Control({
             keys:[
                 { code:props.control.jump, cb:() => {
+                    if (this._gameOver == true) return
                     this.player.Jump(this.render.Size)
-                }}
+                }},
+                { code:"KeyR", cb:() => {
+                    if (this._gameOver == false) return
+                    this.Reset()
+                } }
             ]
         })
         props.element.appendChild(this.render.Element)
@@ -230,26 +345,101 @@ class Game {
 
     }
 
+    private Reset(): void {
+
+        this._gameOver = false
+        this.player.Reset()
+        this._frames.addObstacleSpeed = 0
+        this._frames.spawnObstacle = 0
+        this._opticObstacle.speed.value = this._opticObstacle.speed.default
+        this._obstacles = []
+    }
+
     public Refresh(): void {
 
         if (!this._on) throw ""
-
-        this.player.Refresh(this.render.Size)
-        this.control.Refresh()
-        
-        //Render
-        this.render.Background("#202124")
-        this.render.Player(this.player)
+        this.Logic()
+        this.Render()
     }
 
+    private Logic(): void {
+
+        this.control.Refresh()
+
+        if (this._gameOver) return
+
+        this._frames.addObstacleSpeed ++
+        this._frames.spawnObstacle ++
+
+        if (this._frames.spawnObstacle >= this._opticObstacle.spawn.frame) {
+            this._frames.spawnObstacle = 0
+            
+            let random = Math.floor(Math.random() * 3)
+
+            this._opticObstacle.spawn.value ++
+            if (random == 0 || this._opticObstacle.spawn.value > this._opticObstacle.spawn.max) {
+                this._opticObstacle.spawn.value = 0
+                this.AddObstacle()
+            }
+
+        }
+
+        if (this._frames.addObstacleSpeed >= this._opticObstacle.speed.frame) {
+            this._frames.addObstacleSpeed = 0
+            this._opticObstacle.speed.value = this._opticObstacle.speed.value >= this._opticObstacle.speed.max?this._opticObstacle.speed.max:this._opticObstacle.speed.value +this._opticObstacle.speed.add
+        }
+
+        let obstacles:Obstacle[] = []
+        this._obstacles.forEach((obstacle, index) => {
+            if (obstacle.X + obstacle.Width > 0) { 
+                obstacle.Refresh(-this._opticObstacle.speed.value)
+                obstacles.push(obstacle)
+
+                if (this.player.Collision(obstacle)) {
+                    this._gameOver = true
+                }
+            }
+        })
+        this._obstacles = obstacles
+        obstacles = []
+
+        this.player.Refresh(this.render.Size)
+    }
+
+    private Render(): void {
+
+        this.render.Background("#202124")
+
+        if (this._gameOver) {
+            this.render.GameOver("red")
+            return
+        }
+
+        this.render.Player(this.player)
+        this._obstacles.forEach((obstacle) => { this.render.Obstacle(obstacle) })
+    }
+
+    private AddObstacle(): void {
+
+        if (!this._on) throw ""
+
+        this._obstacles.push(new Obstacle({
+            position:{
+                y:this.render.Height - this._opticObstacle.size.height,
+                x:this.render.Width
+            },
+            size:this._opticObstacle.size
+        }))
+
+    }
 }
  
 const game = new Game({
     playerProps:{
         position:{ x:50, y:50 },
         size:{ width:50, height:50 },
-        force:16,
-        gravity:0.8
+        force:14,
+        gravity:0.6
     },
     renderProps:{
         size:{
@@ -259,6 +449,22 @@ const game = new Game({
     },
     control:{
         jump:"Space"
+    },
+    obstacleOption: {
+        speed:{
+            value:5,
+            max:30,
+            add:1,
+            frame:250
+        },
+        spawn: {
+            frame:110,
+            max:2
+        },
+        size:{
+            width:50,
+            height:50
+        }
     },
     element:document.querySelector("main") || document.body
 })
